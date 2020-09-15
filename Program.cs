1951a8641f93f4935c9566fb4bc411c03bea9966
Toxicann <you@example.com>
2020-09-15 09:15:10 +0545

    after learning static

--- Program.cs
+++ Program.cs
@@ -4,61 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Program program = new Program();
-        string ifcont = "N";
+        string ifcont;
 
-        for (; ; )   //Forever loop.. only exits if a certain condition is met
+        do
         {
             Console.WriteLine("Choose:\n 1. Simple Interest\n 2. Compound Interest\n 3. Area Of Circle\n 4. Volume of Sphere\n 5. BMI calculation\n 6. Gravitational Force\n 7. MassEnergy Equivalence");
             int switch_on = Int16.Parse(Console.ReadLine());
             switch (switch_on)
             {
                 case 1:
-                    program.SimpleInterest();
+                    SimpleInterest();
                     break;
 
                 case 2:
-                    program.CompoundInterest();
+                    CompoundInterest();
                     break;
 
                 case 3:
-                    program.AreaOCircle();
+                    AreaOCircle();
                     break;
 
                 case 4:
-                    program.VolOSphere();
+                    VolOSphere();
                     break;
 
                 case 5:
-                    program.BmiCalc();
+                    BmiCalc();
                     break;
 
                 case 6:
-                    program.GravitationalForce();
+                    GravitationalForce();
                     break;
 
                 case 7:
-                    program.MassEngergy();
+                    MassEngergy();
                     break;
 
                 default:
                     Console.WriteLine("Invalid argument");
                     break;
             }
-            Console.WriteLine("Continue..? Y/N");
+            Console.WriteLine("Continue..? Y..?");
             ifcont = Console.ReadLine();
             ifcont = ifcont.ToUpper();
 
-            if (ifcont == "Y")
-                continue;
-            else
-                Environment.Exit(1);  // condition to exit from forever loop
-        }
+        }while(ifcont == "Y");
 
     }
 
 
-    void SimpleInterest()
+    static void SimpleInterest()
     {
         double principle, time, rate, interest;
 
@@ -72,7 +67,7 @@ class Program
         Console.WriteLine("Simple Interest is: {0} ", interest);
     }
 
-    void CompoundInterest()
+    static void CompoundInterest()
     {
         double principle, time, rate, interest, compound;
 
@@ -90,7 +85,7 @@ class Program
         Console.WriteLine("Compound Interest is: {0} ", interest);
     }
 
-    void AreaOCircle()
+    static void AreaOCircle()
     {
         Console.WriteLine("Enter Radius ");
         double radius = Convert.ToDouble(Console.ReadLine());
@@ -100,7 +95,7 @@ class Program
         Console.WriteLine("Area of Circle is: {0} ", area);
     }
 
-    void VolOSphere()
+    static void VolOSphere()
     {
         Console.WriteLine("Enter Radius ");
         double radius = Convert.ToDouble(Console.ReadLine());
@@ -110,7 +105,7 @@ class Program
         Console.WriteLine("Volume of Sphere is: {0} ", volume);
     }
 
-    void BmiCalc()
+    static void BmiCalc()
     {
         Console.WriteLine("Enter your weight in kg & height in cm: ");
         double weight = Convert.ToDouble(Console.ReadLine());
@@ -123,7 +118,7 @@ class Program
         Console.WriteLine("BMI is {0}", bmi);
     }
 
-    void BillPPerson()
+    static void BillPPerson()
     {
         Console.WriteLine("Enter The Bill Amount and the no. of persons to split the bill with: ");
         double amount = Convert.ToDouble(Console.ReadLine());
@@ -134,7 +129,7 @@ class Program
         Console.WriteLine("Bill Per Person is: {0}", billpperson);
     }
 
-    void GravitationalForce()
+    static void GravitationalForce()
     {
         double kGravitationalForceConst = 6.674 * Math.Pow(10, -11);
         Console.WriteLine("Enter the value of Mass1, Mass2 in kg & distance betn these masses in m");
@@ -149,7 +144,7 @@ class Program
 
     }
 
-    void MassEngergy()
+    static void MassEngergy()
     {
         double speedoLight = 3 * Math.Pow(10, 8);
         Console.WriteLine("Enter the value of Mass in kg");
